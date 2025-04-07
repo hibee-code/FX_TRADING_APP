da59af6bbaf805b5f2c722c090979c26e8ff0c71
ibrahim <okeibrahim917@gmail.com>
2025-04-07 17:00:28 +0100

    Remove .env and dist from repository

--- dist/config/database/db.config.d.ts
+++
@@ -1,5 +0,0 @@
-import { DataSource, DataSourceOptions } from 'typeorm';
-import 'dotenv/config';
-export declare const dataSourceOptions: DataSourceOptions;
-declare const dataSource: DataSource;
-export default dataSource;
--- dist/config/helpers/general.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.pathFromSrc = void 0;
-const path_1 = require("path");
-const pathFromSrc = (path) => {
-    return (0, path_1.join)(__dirname, '../../', path);
-};
-exports.pathFromSrc = pathFromSrc;
-//# sourceMappingURL=general.js.map
